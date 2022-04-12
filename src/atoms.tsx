ce61101949db08c5110123f8e0370a89b8fcb0c2
Choi Kyuhyeon <bluejet1104@gmail.com>
2022-04-13 01:45:11 +0900

    selector 적용중

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export interface IToDo {
   text: string;
@@ -10,3 +10,11 @@ export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
 });
+
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({get}) => {
+    const toDos = get(toDoState)
+    return "hello";
+  }
+})
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,11 +1,13 @@
 import { useForm } from "react-hook-form";
 import { useRecoilState, useRecoilValue } from "recoil";
-import { toDoState } from "../atoms";
+import { toDoSelector, toDoState } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function ToDoList() {
   const toDos = useRecoilValue(toDoState);
+  const selectorOutput = useRecoilValue(toDoSelector)
+  console.log(selectorOutput)
   return (
     <div>
       <h1>To dos</h1>

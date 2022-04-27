5299cf09fb458396db4170da01cbe58beae15ebc
최규현 사원/블록체인LAB <kyuhyeon@lgcns.com>
2022-04-27 23:03:49 +0900

    drag example completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,57 @@
-import { motion } from "framer-motion";
-import { useRef } from "react";
-import styled from "styled-components";
+import { motion } from 'framer-motion';
+import { useRef } from 'react';
+import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 
 const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.4);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
+    width: 600px;
+    height: 600px;
+    background-color: rgba(255, 255, 255, 0.4);
+    border-radius: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
 `;
 
 const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0, 10px, 20px rgba(0, 0, 0, 0.06);
+    width: 200px;
+    height: 200px;
+    background-color: rgba(255, 255, 255, 1);
+    border-radius: 40px;
+    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0, 10px, 20px rgba(0, 0, 0, 0.06);
 `;
 
 const boxVariants = {
-  hover: { rotateZ: 90 },
-  click: { borderRadius: "100px" },
+    hover: { rotateZ: 90 },
+    click: { borderRadius: '100px' },
 };
 
 function App() {
-  const biggerBoxRef = useRef<HTMLDivElement>(null);
-  return (
-    <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          drag
-          dragConstraints={{ top: -200, bottom: 200, left: -200, right: 200 }}
-          variants={boxVariants}
-          whileHover="hover"
-          whileDrag="drag"
-          whileTap="click"
-        ></Box>
-      </BiggerBox>
-    </Wrapper>
-  );
+    const biggerBoxRef = useRef<HTMLDivElement>(null);
+    return (
+        <Wrapper>
+            <BiggerBox ref={biggerBoxRef}>
+                <Box
+                    drag
+                    dragSnapToOrigin
+                    dragElastic={0.5}
+                    dragConstraints={biggerBoxRef}
+                    variants={boxVariants}
+                    whileHover='hover'
+                    whileDrag='drag'
+                    whileTap='click'
+                ></Box>
+            </BiggerBox>
+        </Wrapper>
+    );
 }
 
 export default App;

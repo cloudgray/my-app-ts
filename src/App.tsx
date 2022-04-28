732b9a2fc8fb449e4031c6c777cd21caa1b7f2cd
최규현 사원/블록체인LAB <kyuhyeon@lgcns.com>
2022-04-28 09:39:16 +0900

    scroll applied

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import { motion } from 'framer-motion';
-import { useRef } from 'react';
+import {
+    motion,
+    useMotionValue,
+    useTransform,
+    useViewportScroll,
+} from 'framer-motion';
+import { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
-    height: 100vh;
+const Wrapper = styled(motion.div)`
+    height: 200vh;
     width: 100vw;
     display: flex;
     justify-content: center;
     align-items: center;
-`;
-
-const BiggerBox = styled.div`
-    width: 600px;
-    height: 600px;
-    background-color: rgba(255, 255, 255, 0.4);
-    border-radius: 40px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    overflow: hidden;
 `;
 
 const Box = styled(motion.div)`
@@ -26,7 +20,7 @@ const Box = styled(motion.div)`
     height: 200px;
     background-color: rgba(255, 255, 255, 1);
     border-radius: 40px;
-    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0, 10px, 20px rgba(0, 0, 0, 0.06);
+    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0, 10px, 20px rgba(0, 0, 0, 0.06);s
 `;
 
 const boxVariants = {
@@ -35,21 +29,29 @@ const boxVariants = {
 };
 
 function App() {
-    const biggerBoxRef = useRef<HTMLDivElement>(null);
+    const x = useMotionValue(0);
+    const scale = useTransform(x, [-800, 800], [2, 0.5]);
+    const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+    const gradient = useTransform(
+        x,
+        [-800, 0, 800],
+        [
+            'linear-gradient(135deg, #59c173, #a17fe0, #5d26c1)',
+            'linear-gradient(135deg, rgba(198,255,221,1.0), rgba(251,215,134,1.0), rgba(247,121,125,1.0))',
+            'linear-gradient(to right, #77a1d3, #79cbca, #e684ae)',
+        ]
+    );
+    const { scrollY, scrollYProgress } = useViewportScroll();
+
+    useEffect(() => {
+        x.onChange(() => console.log(rotateZ.get()));
+        scrollY.onChange(() => console.log(scrollY.get()));
+        scrollYProgress.onChange(() => console.log(scrollYProgress.get()));
+    }, [x, scrollY]);
+
     return (
-        <Wrapper>
-            <BiggerBox ref={biggerBoxRef}>
-                <Box
-                    drag
-                    dragSnapToOrigin
-                    dragElastic={0.5}
-                    dragConstraints={biggerBoxRef}
-                    variants={boxVariants}
-                    whileHover='hover'
-                    whileDrag='drag'
-                    whileTap='click'
-                ></Box>
-            </BiggerBox>
+        <Wrapper style={{ background: gradient }}>
+            <Box style={{ x, scale, rotateZ }} drag='x' dragSnapToOrigin />
         </Wrapper>
     );
 }
